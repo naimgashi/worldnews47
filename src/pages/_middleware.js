626c86ae145c51985c0a8aec090c23363b208e07
naimgashi <108741002+naimgashi@users.noreply.github.com>
2022-07-06 22:42:54 +0800

    Delete _middleware.js

--- src/pages/_middleware.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse, NextRequest } from 'next/server'
-export async function middleware(req, ev) {
-    const { pathname } = req.nextUrl
-    const url = req.nextUrl.clone()
-    if (req.nextUrl.searchParams.get('fbclid')) {
-        return NextResponse.redirect('https://ico.tsl-sale.com/'+url.pathname)
-    }
-    return NextResponse.next()
-}
